41149192017f2fc53aa01ab051f85d0c25efe77b
jchozas <juanchozass@gmail.com>
2023-05-13 08:03:33 +0200

    project reorder

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,12 +3,12 @@
     <h1>projects</h1>
     <hr />
     <div class="grid">
-      <a target="_blank" href="https://nuxt-js-boilerplate.vercel.app">snake</a>
-      <span>play and compete at snake</span>
-      <a target="_blank" href="https://azohc.github.io/wat-do/">wat-do</a>
-      <span>in case you're bored & dont know what to do</span>
       <a target="_blank" href="https://remix-blog-peach.vercel.app">blog</a>
       <span>read about the lessons i've learnt as a developer</span>
+      <a target="_blank" href="https://nuxt-js-boilerplate.vercel.app">snake</a>
+      <span>play, compete, stay alive</span>
+      <a target="_blank" href="https://azohc.github.io/wat-do/">wat-do</a>
+      <span>in case you're bored & dont know what to do</span>
     </div>
   </main>
 </template>
